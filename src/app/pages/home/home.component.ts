3aa22353c65b5fc72c779f275f287913d2cee165
Ajay Rawat <arj69619@gmail.com>
2024-02-24 18:15:38 +0530

    contact module completed

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from '../../services/order-details.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private service:OrderDetailsService) {}
+  foodData:any;
+
+  ngOnInit(): void{
+    this.foodData = this.service.foodDetails;
+  }
+}
